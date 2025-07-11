3bdadb7a251073d6e887e1486771ad17252d283c
Lewis-Mukanji <kedeiyelewis@gmail.com>
2025-07-11 17:21:00 +0300

    env and mailer changes

--- backend-project/mailer.js
+++ backend-project/mailer.js
@@ -4,11 +4,16 @@ const path = require('path');
 require('dotenv').config();
 
 const transporter = nodemailer.createTransport({
-  service: 'gmail',
+  host: 'smtp.gmail.com',
+  port: 587,
+  secure: false, // true for 465, false for other ports
   auth: {
     user: process.env.EMAIL_USER,
     pass: process.env.EMAIL_PASS,
   },
+  tls: {
+    rejectUnauthorized: false // This helps with some connection issues
+  }
 });
 
 const getEmailTemplate = async (templateData, templateType = 'contact') => {
