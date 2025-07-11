70baa3517f9cda22e498dec7f793e864a23d6bc1
Lewis-Mukanji <kedeiyelewis@gmail.com>
2025-07-11 17:35:25 +0300

    env and mailer port change

--- backend-project/mailer.js
+++ backend-project/mailer.js
@@ -5,8 +5,8 @@ require('dotenv').config();
 
 const transporter = nodemailer.createTransport({
   host: 'smtp.gmail.com',
-  port: 587,
-  secure: false, // true for 465, false for other ports
+  port: 456,
+  secure: true, // true for 465, false for other ports
   auth: {
     user: process.env.EMAIL_USER,
     pass: process.env.EMAIL_PASS,
